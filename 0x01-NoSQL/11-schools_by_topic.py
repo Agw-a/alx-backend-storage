5cc1da5bac6e366a11bd9a616e003c2e45ccb17a
Agw-a <lizagwa7@gmail.com>
2023-01-17 22:45:29 +0300

    Task 11: comments

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,14 @@
 
 
 def schools_by_topic(mongo_collection, topic):
+    """returns list of schools with topics
+
+    Args:
+        mongo_collection: pymongo coll. object
+        topic (str): topic to search
+
+    Returns:
+        list with result
+    """
     query_condition = {'topics': {'$eq': topic}}
     return [result for result in mongo_collection.find(query_condition)]
