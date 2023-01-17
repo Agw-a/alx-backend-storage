2fd44fdda58468cac92389686df4103f6e18da06
Agw-a <lizagwa7@gmail.com>
2023-01-17 14:08:04 +0300

    Task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/bin/bash/env python3
+'''Task 8 module
+'''
+
+
+def list_all(mongo_collection):
+    """returns list of all documents
+
+    Args:
+        mongo_collection:pymongo collection object
+    Returns:
+        list: all documents
+    """
+    my_doc = mongo_collection.find()
+    for i in my_doc:
+        return [i]
